e50b370c0abcc647745a3117f7d2be2101750dd6
Xiomara Esmit Pérez Vásquez <xiomara.perez@entelgy.com>
2026-05-27 17:48:22 -0500

    feat: add composition info-field

---
+++ src/compositions/info-field/info-field.js
@@ -0,0 +1,29 @@
+import {litElement, html} from 'lit';
+import {infoFieldStyles} from './info-field.css.js';
+
+export class InfoField extends litElement {
+    static styles = infoFieldStyles
+    static properties = {
+        variant: {type: String},
+    }
+
+    constructor() {
+        super();
+        this.variant = '';
+    }
+
+    render() {
+        return html`
+            <div class="info-field ${this.variant}">
+                <div class="label">
+                <slot name="label"></slot>
+                </div>
+                <div class="value">
+                <slot name="value"></slot>
+                </div>
+            </div>
+        `
+    }
+}
+
+customElements.define('info-field', InfoField);
