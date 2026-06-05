1ae0deaa834e562e72242297e1db5acf1554bd76
eyder-eg <eyder.huayta@entelgy.com>
2026-06-05 06:52:23 -0500

    style(info-card): fix component layout and card styling

--- src/compositions/info-card/info-card.css.js
+++ src/compositions/info-card/info-card.css.js
@@ -6,11 +6,16 @@ export const styles = css`
   :host {
     display: block;
     width: 100%;
+
   }
   .info-card {
+    background-color: white;
+    color: black;
     display: flex;
     width: 100%;
     flex-direction: column;
     padding: 1rem;
+    box-sizing: border-box;
+    box-shadow: var(--shadow-md)
   }
 `;
